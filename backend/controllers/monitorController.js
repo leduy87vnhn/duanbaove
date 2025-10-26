782cf4b0ac11238444fe7a697cf6dcf389870fdb
leduy87vnhn <leduy87vnhn@gmail.com>
2025-10-26 10:38:52 +0700

    update URL backend

--- backend/controllers/monitorController.js
+++ backend/controllers/monitorController.js
@@ -1,5 +1,3 @@
 export const getRTSPLink = (req, res) => {
-  res.json({
-    rtsp: 'rtsp://admin:Abcd121%40@113.185.6.120:8554/Streaming/Channels/101'
-  });
+  res.json({ rtsp: 'http://18.141.204.161:8889/index.m3u8' });
 };
